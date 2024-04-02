e255e22225bb7a10817e81674070cb3c43a86a7e
TeaLeaf115 <91578545+TeaLeaf115@users.noreply.github.com>
2024-04-02 18:49:51 -0400

    Made it so you can use keyboard inputs for some functions

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -12,13 +12,20 @@ public class KeyboardInputs implements KeyListener {
 	public KeyboardInputs(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 	}
+	
 	@SuppressWarnings("incomplete-switch")
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (GameStates.state) {
-//			case MAIN_MENU -> gamePanel.getGame().getMenu().keyReleased(e);
+			case STARTUP -> gamePanel.getGame().getStartup().keyReleased(e);
+			case MAIN_MENU -> gamePanel.getGame().getMenu().keyReleased(e);
+			case DIFF_SELECTION -> gamePanel.getGame().getDiffSelector().keyReleased(e);
 			case PLAY -> gamePanel.getGame().getPlaying().keyReleased(e);
-//			case CREDITS -> gamePanel.getGame().getCredits().keyReleased(e);
+			case INVENTORY, MAP -> {}
+			case CLUES -> gamePanel.getGame().getClueScreen().keyReleased(e);
+			case OPTIONS -> gamePanel.getGame().getOptions().keyReleased(e);
+			case CREDITS -> gamePanel.getGame().getCredits().keyReleased(e);
+			case NEW_GAME_BLURB -> gamePanel.getGame().getNewGameBlurb().keyReleased(e);
 		}
 	}
 	
@@ -26,14 +33,18 @@ public class KeyboardInputs implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (GameStates.state) {
-//			case MAIN_MENU -> gamePanel.getGame().getMenu().keyPressed(e);
+			case STARTUP -> gamePanel.getGame().getStartup().keyPressed(e);
+			case MAIN_MENU -> gamePanel.getGame().getMenu().keyPressed(e);
+			case DIFF_SELECTION -> gamePanel.getGame().getDiffSelector().keyPressed(e);
 			case PLAY -> gamePanel.getGame().getPlaying().keyPressed(e);
-//			case OPTIONS -> gamePanel.getGame().getGameOptions().keyPressed(e);
+			case OPTIONS -> gamePanel.getGame().getOptions().keyPressed(e);
+			case INVENTORY, MAP -> {}
+			case CLUES -> gamePanel.getGame().getClueScreen().keyPressed(e);
+			case CREDITS -> gamePanel.getGame().getCredits().keyPressed(e);
+			case NEW_GAME_BLURB -> gamePanel.getGame().getNewGameBlurb().keyPressed(e);
 		}
 	}
 	
 	@Override
-	public void keyTyped(KeyEvent e) {
-		// Not In Use
-	}
+	public void keyTyped(KeyEvent e) {}
 }
